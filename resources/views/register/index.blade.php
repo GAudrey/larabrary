72b03d6b0cea8cd83e4f3f967c448806c6406e47
GAudrey <gilmantaudrey.ag@gmail.com>
2021-02-11 11:36:48 +0100

    some rename, add register

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           {{ __('Library') }}
+           {{ __('Register') }}
         </h2>
     </x-slot>
 
@@ -9,25 +9,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-                <a href="{{ route('books.create')}}">Create a new entry</a>
+                <a href="{{ route('users.create')}}">Create a new member</a>
 
                 <table>
                 <tr>   
                     <th>ID</th>
-                    <th>Authors</th>
-                    <th>Titles</th>
-                    <th>Genres</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Since</th>
+                    <th>Management</th>
                 </tr>
-                @foreach($books as $book)
+                @foreach($users as $user)
                 <tr>
-                    <td>{{ $book->id }}</td>
-                    <td>{{ $book->author }}</td>
-                    <td>{{ $book->title }}</td>
-                    <td>{{ $book->genre }}</td>
+                    <td>{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td>{{ $user->role }}</td>
+                    <td>{{ $user->create_at }}</td>
                     <td>
-                        <a href="{{ route('books.show', $book->id) }}">Display</a>
-                        <a href="{{ route('books.edit', $book->id) }}">Edit</a>
-                        <form class="inline-block" action="{{ route('books.destroy', $book->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
+                        <a href="{{ route('users.show', $user->id) }}">Display</a>
+                        <a href="{{ route('users.edit', $user->id) }}">Edit</a>
+                        <form class="inline-block" action="{{ route('users.destroy', $user->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
                             <input type="hidden" name="_method" value="DELETE">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
                             <input type="submit" class="text-red-600 bg-transparent hover:text-red-900 mb-2 mr-2" value="Delete">
