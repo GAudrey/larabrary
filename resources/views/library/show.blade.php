b1eb75f41ef01db39efe5940373bd6472ff3384f
GAudrey <gilmantaudrey.ag@gmail.com>
2021-02-11 10:30:26 +0100

    delete done

--- resources/views/library/show.blade.php
+++ resources/views/library/show.blade.php
@@ -21,8 +21,15 @@
                     <td>{{ $book->author }}</td>
                     <td>{{ $book->title }}</td>
                     <td>{{ $book->genre }}</td>
-                    <td><a href="{{ route('books.edit', $book->id) }}">Edit</a></td>
-                    <td><a href="{{ route('books.destroy', $book->id) }}">Delete</a></td>
+                    <td>
+                        <a href="{{ route('books.show', $book->id) }}">Display</a>
+                        <a href="{{ route('books.edit', $book->id) }}">Edit</a>
+                        <form class="inline-block" action="{{ route('books.destroy', $book->id) }}" method="POST" onsubmit="return confirm('Are you sure?');">
+                            <input type="hidden" name="_method" value="DELETE">
+                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                            <input type="submit" class="text-red-600 bg-transparent hover:text-red-900 mb-2 mr-2" value="Delete">
+                        </form>
+                    </td>
                 </tr>
                 </table>
             </div>
